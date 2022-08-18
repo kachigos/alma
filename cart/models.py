74c90aed77c7d2c24a72fe2e573d3b2859c7213e
kachigos <kachigos@C02WF085HV2T.grubhub.local>
2022-08-18 13:02:40 +0600

    .

--- cart/models.py
+++ cart/models.py
@@ -1,9 +1,11 @@
-from django.contrib.auth.models import User
+# from django.contrib.auth.models import User
 from django.core.validators import MinValueValidator
 from django.db import models
 
 from product.models import Product
+from django.contrib.auth import get_user_model
 
+User = get_user_model()
 
 class OrderManager(models.Manager):
 
