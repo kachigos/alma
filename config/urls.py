129f431dbb24694ba027b87c4b88eaf21e7fe922
kachigos <kachigos@C02WF085HV2T.grubhub.local>
2022-08-19 12:44:27 +0600

    .

--- config/urls.py
+++ config/urls.py
@@ -15,11 +15,23 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title = 'ALLMA API',
+        description = 'URLки маркетплейса ALMA',
+        default_version = 'v1'
+    ),
+    public = True
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
     path('', include('product.urls')),
     path('', include('comments.urls')),
+    path('docs/', schema_view.with_ui('swagger')),
 
 ]
